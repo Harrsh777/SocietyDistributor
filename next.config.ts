6ee72e4c004fc7679ceb9e2b116697109ed3c5a7
Harrsh <harrshh077@gmail.com>
2025-06-22 16:13:58 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   experimental: {
     turbo: {
       resolveExtensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
   },
    images: {
-    domains: ['images.unsplash.com', 'companieslogo.com', "via.placeholder.com"],
+    domains: ['images.unsplash.com', 'companieslogo.com', "via.placeholder.com", "nrdpnjrnnwvlajqgxphw.supabase.co"],
   },
   /* config options here */
 };
